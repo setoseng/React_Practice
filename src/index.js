e426c404aa834aa0bddb2d825db9658f44de5f59
setoseng <setoseng@gmail.com>
2017-07-24 16:20:56 -0500

    more practice with react and testing

--- src/index.js
+++ src/index.js
@@ -10,10 +10,16 @@ class App extends Component{
   constructor(props){
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null,
+     };
 
-    YTSearch({key: API_KEY, term:'surfboards'}, (videos) => {
-      this.setState({ videos });
+    YTSearch({key: API_KEY, term:'kendrick lamar'}, (videos) => {
+      this.setState({
+        videos:videos,
+        selectedVideo:videos[0],
+       });
   //  this.setState({videos: videos}); ONLY WORKS WHEN KEY AND PROPERTY ARE SAME
     });
   }
@@ -22,8 +28,10 @@ class App extends Component{
     return(
       <div>
         <SearchBar />
-        <VideoDetail video={this.state.videos[0]} />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+          videos={this.state.videos} />
       </div>
     );
   }
